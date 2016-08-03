23b2ff30c41db3be8c2441d4090cdf7423ee30a8
Tommy Nguyen <tommy@rcd.cool>
2016-08-03 08:46:32 -0400

    Lesson Plan

---
+++ assets/js/lessons.js
@@ -0,0 +1,72 @@
+// Comments
+/*
+ Another comment
+ * */
+
+// JavaScript basic types
+
+
+// Object type in JavaScript - JSON
+// Bonus: What is JSON?
+
+
+// Accessing object properties
+
+// Bonus: What if there's an object inside an object?
+
+// Arrays
+
+// Truthy and falsey
+// Bonus: Name three different falsey values
+
+// Function notation
+
+// Callbacks - set timeout
+
+/**
+ * Executes callback after a time.
+ * @param time
+ * @param callback
+ */
+
+// doSomethingAfterTime
+
+// Classes - Object Oriented
+
+/*
+
+    // In some other programming language you might have something like:
+
+    class Task {
+        private content;
+
+        Task(string contentIn) {
+            // The constructor
+            this.content = contentIn;
+        }
+
+        public void log () {
+            cout << this.content;
+        }
+    }
+ */
+
+// But in JS:
+
+// Log Task method on Task class
+
+// New Task
+
+// Document Object Model
+
+// Accessing a node on the document
+
+// Changing the node on the document
+
+// Creating a node on the document
+
+// Removing a node
+
+// Events - clicking a button
+
+// Event listener
